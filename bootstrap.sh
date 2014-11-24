b5013e0b50e848a7e74155028030f99b472fe6d1
Himanshu Jindal <himanshujindal@gmail.com>
2014-11-24 06:07:46 +0000

    Modify bootstrap.sh to include more bootstrap commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,11 @@
 ln -s ~/configs/,vimrc ~/.vimrc
 ls -s ~/comfigs/.bashrc ~/.bashrc
+sudo yum update
+sudo yum install git
+git config --global user.name "Himanshu Jindal"
+git config --global user.email "himanshujindal@gmail.com"
+git clone https://github.com/himanshujindal/configs.git
+cd configs
+git branch --set-upstream-to=origin/master master
+git fetch --all
+git reset --hard origin/master
