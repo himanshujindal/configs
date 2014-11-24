b0d3aec010e00a8c995d17ab01cd8beca10fada1
Himanshu Jindal <himanshujindal@gmail.com>
2014-11-24 06:13:34 +0000

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,5 @@
-ln -s ~/configs/,vimrc ~/.vimrc
-ls -s ~/comfigs/.bashrc ~/.bashrc
-sudo yum update
-sudo yum install git
+ln -s ~/configs/,vimrc ~/
+ls -s ~/configs/.bashrc ~/
 git config --global user.name "Himanshu Jindal"
 git config --global user.email "himanshujindal@gmail.com"
-git clone https://github.com/himanshujindal/configs.git
-cd configs
 git branch --set-upstream-to=origin/master master
-git fetch --all
-git reset --hard origin/master
