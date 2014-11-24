5d81f2cc7f7cbe97d273b2e0db1ca41dfe00f40d
Himanshu Jindal <himanshujindal@gmail.com>
2014-11-24 06:16:27 +0000

    Fix bootstrap some more after testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
-ln -s ~/configs/,vimrc ~/
-ls -s ~/configs/.bashrc ~/
+#!/bin/bash
+ln -s ~/configs/.vimrc ~/
+ln -s ~/configs/.bashrc ~/
 git config --global user.name "Himanshu Jindal"
 git config --global user.email "himanshujindal@gmail.com"
 git branch --set-upstream-to=origin/master master
